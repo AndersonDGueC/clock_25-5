fef00908fd96b4859528553d85af2ed1775d6b85
anderson <adguerreroc@unal.edu.co>
2022-09-27 14:27:52 -0500

    indexitation Container to app and configured bootstrap and sass style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import ContainerChron from './components/ContainerChron'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './style/clock_25_5.css'
+
 function App() {
   return (
-    <div>
-	hola
-    </div>
+    <div className='container-fluid'>
+     <div className='d-flex justify-content-center align-items-center'>
+	<div classname='col-xs-6 p-5 rounded box'>
+	<div classname='mb-4'>
+	<ContainerChron/>
+	</div>
+    	</div>
+      </div>
+     </div>	
   );
 }
 
---
+++ src/components/ContainerChron.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const ContainerChron=()=>{
+
+return(
+	<div>Hi Started</div>
+)
+}
+
+export default 	ContainerChron;
