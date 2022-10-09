e8ee978cce66ed6354d41a217605b3f3c6484c1b
anderson <adguerreroc@unal.edu.co>
2022-10-09 17:54:10 -0500

    add clockFunction file

---
+++ src/components/store/clockFunction.js
@@ -0,0 +1,25 @@
+import {INC_SESS, INC_BREK, DEC_SESS, DEC_BREK, START_STOP, RESET}
+import initialStateClock from './initialStateClock'
+
+export const reducerClock=(state, action)=>{
+	switch(action.type){
+	case INC_SESS:
+	alert("Hola incrementar session")
+	
+	case INC_BREK:
+
+	case DEC_SESS:
+
+
+	case DEC_BREK:
+
+
+	case START_STOP:
+
+	case RESET:
+
+	default:
+	   return initialStateClock
+	}
+}
+
